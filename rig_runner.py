5023efad47f855b350c8cd1933d85a3ba410b6c4
bp100a <bp100a@hotmail.com>
2018-10-28 18:31:46 -0400

    wire up 2nd optical switch

--- rig_runner.py
+++ rig_runner.py
@@ -17,8 +17,8 @@ CAMERA_STEPPER_MOTOR_SPEED = 120  # rpm
 MODEL_STEPPER_MOTOR_NUM = 1
 MOTOR_HAT_I2C_ADDR = 0x6F
 MOTOR_HAT_I2C_FREQ = 1600
-CCW_MAX_SWITCH = limit_switch.LimitSwitch(4) # furthest CCW rotation allowed
-CW_MAX_SWITCH = limit_switch.LimitSwitch(17) # furthest CW rotation allowed
+CCW_MAX_SWITCH = limit_switch.LimitSwitch(4)  # furthest CCW rotation allowed
+CW_MAX_SWITCH = limit_switch.LimitSwitch(13)  # furthest CW rotation allowed
 
 
 def yield_function(direction: int) -> bool:
