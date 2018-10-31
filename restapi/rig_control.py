07892b37a0f506d60ccda067499847c957a4c626
bp100a <bp100a@hotmail.com>
2018-10-31 14:21:14 -0400

    print out exception

--- restapi/rig_control.py
+++ restapi/rig_control.py
@@ -170,7 +170,7 @@ def rig_status():
         else:
             return make_response(status_json, status.HTTP_200_OK)
     except Exception as e:
-        return make_response("something really bad! {0}".format(e.__str__), status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return make_response("something really bad -> {0}".format(e.__str__()), status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @app.route("/home", methods=['POST'])
